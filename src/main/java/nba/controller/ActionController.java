bf05babc35acf748ab19a00fc66c17c6840f5cdf
Seesiz <108148026+Seesiz@users.noreply.github.com>
2023-11-21 12:03:17 +0300

    ny ARO

---
+++ src/main/java/nba/controller/ActionController.java
@@ -0,0 +1,27 @@
+package nba.controller;
+
+import nba.model.Action;
+import nba.service.ActionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/action")
+public class ActionController {
+    private ActionService actionService;
+
+    @Autowired
+    public ActionController(ActionService actionService) {
+        this.actionService = actionService;
+    }
+
+    @GetMapping
+    public List<Action> getAction(){
+        return  actionService.getAction();
+    }
+
+}
